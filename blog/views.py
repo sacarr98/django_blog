e1b9257dbc7cf971faaa3dc443a2903081441eac
sacarr98 <>
2024-01-20 12:44:43 +0000

    add css styling

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,6 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6

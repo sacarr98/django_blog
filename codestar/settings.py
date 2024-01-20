3ae7405341b6a495555655482e00e1e5676b2ee7
sacarr98 <>
2024-01-20 11:10:52 +0000

    add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0ku1g23bymvm%qbpm2_iulxy7c4pnnc7zzywnap@68^_-it9bi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["8000-sacarr98-django-blog-qx37cx2mvl.us2.codeanyapp.com", ".herokuapp.com"]
+ALLOWED_HOSTS = [
+    "8000-sacarr98-django-blog-qx37cx2mvl.us2.codeanyapp.com", ".herokuapp.com"]
 
 
 # Application definitions
@@ -88,6 +89,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation

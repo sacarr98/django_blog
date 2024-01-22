f7af3414360e5b871e8f3a9fd1a6bf77d7793eae
sacarr98 <>
2024-01-22 12:05:34 +0000

    add functionaility to delete comments

--- about/views.py
+++ about/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .fields import CollaborateForm
 
 
 def about_me(request):
     """
     Renders the About page
     """
-    about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request,
+                                 messages.SUCCESS,
+                                 "Collaboration request recieved! I endeavor to respond within 2 working days.")
 
-    return render(request, "about/about.html", {"about": about},)
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
+    return render(request, "about/about.html", {
+        "about": about,
+        "collaborate_form": collaborate_form
+    },
+    )
